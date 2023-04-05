b27bbd4539c3ddc283e0181fc61ee9bdece61ba3
Bruno Coelho <eu.brunocoelho94@gmail.com>
2023-04-05 16:41:10 -0300

    route group created

--- app/routes.php
+++ app/routes.php
@@ -6,16 +6,30 @@ use Psr\{
 };
 
 use Slim\{
-    App
+    App,
+    Routing\RouteCollectorProxy
 };
 
 use App\Controller\{
-    LoginController
+    LoginController,
+    DashboardController
 };
 
 return function (App $app) {
-    $app->get('/', [LoginController::class, 'login']);
-    $app->get('/login', [LoginController::class, 'login'])->setName('login');
-    $app->post('/login', [LoginController::class, 'login']);
-    $app->get('/dashboard', [DashboardController::class, 'index'])->setName('dashboard');
+    $app->group('', function (RouteCollectorProxy $group) {
+        $group->get('/', [LoginController::class, 'login']);
+        $group->get('/login', [LoginController::class, 'login'])->setName('login');
+        $group->post('/login', [LoginController::class, 'login']);
+    });
+
+    $app->group('/dashboard', function (RouteCollectorProxy $group) {
+        $group->get('', [DashboardController::class, 'index'])->setName('dashboard');
+    });
 };
+
+// return function (App $app) {
+//     $app->get('/', [LoginController::class, 'login']);
+//     $app->get('/login', [LoginController::class, 'login'])->setName('login');
+//     $app->post('/login', [LoginController::class, 'login']);
+//     $app->get('/dashboard', [DashboardController::class, 'index'])->setName('dashboard');
+// };
---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Psr\{
+    Http\Message\ResponseInterface as Response,
+    Http\Message\ServerRequestInterface as Request
+};
+
+use Slim\{
+    App,
+    Views\PhpRenderer
+};
+
+class DashboardController
+{
+    private
+        $app, $container, $renderer;
+
+    public function __construct(App $app)
+    {
+        $this->app = $app;
+        $this->container = $this->app->getContainer();
+        $this->renderer = $this->container->get(PhpRenderer::class);
+    }
+
+    public function index(Request $request, Response $response, array $args): Response
+    {
+        return $this->renderer->render($response, 'dashboard/index.php');
+    }
+}
